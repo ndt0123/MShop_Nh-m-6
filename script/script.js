4093dd86a6b283678d09bcda2d70637ff47eda75
ndt0123 <nguyenduytam01231999@gmail.com>
2019-10-22 09:53:33 +0700

    update project

--- script/script.js
+++ script/script.js
@@ -28,17 +28,116 @@ $(document).ready(function() {
 	});
 
 	/*Hiệu ứng khi di chuột lên sản phẩm*/
+	
 	$(".prds").mouseover(function() {
+		$(this).css("box-shadow", "0px 0px 10px 0px #a7a7a7");
+		$(this).css("border-radius", "3px");
 		$(this).children("h5").css("color", "blue");
-	});
-	$(".prds").mouseout(function() {
-		$(this).children("h5").css("color", "black");
-	});
-
-	$(".prds").mouseover(function() {
 		$(this).children("b").css("color", "red");
 	});
 	$(".prds").mouseout(function() {
+		$(this).css("box-shadow", "none");
+		$(this).css("border-radius", "0px");
+		$(this).children("h5").css("color", "black");
 		$(this).children("b").css("color", "#b10000");
 	});
+
+	/*Hiển thị các thuộc tính của bộ lọc trong phần kết quả tìm kiếm*/
+	$("#display-phone-company-filter-btn").click( function() {
+		$(this).css("display", "none");
+		$("#undisplay-phone-company-filter-btn").css("display", "block");
+		$("#phone-company-filter").css("display", "block");
+	});
+	$("#display-phone-price-filter-btn").click( function() {
+		$(this).css("display", "none");
+		$("#undisplay-phone-price-filter-btn").css("display", "block");
+		$("#phone-price-filter").css("display", "block");
+	});
+	$("#display-phone-operating-sys-filter-btn").click( function() {
+		$(this).css("display", "none");
+		$("#undisplay-phone-operating-sys-filter-btn").css("display", "block");
+		$("#phone-operating-sys-filter").css("display", "block");
+	});
+
+	$("#undisplay-phone-company-filter-btn").click( function() {
+		$(this).css("display", "none");
+		$("#display-phone-company-filter-btn").css("display", "block");
+		$("#phone-company-filter").css("display", "none");
+	});
+	$("#undisplay-phone-price-filter-btn").click( function() {
+		$(this).css("display", "none");
+		$("#display-phone-price-filter-btn").css("display", "block");
+		$("#phone-price-filter").css("display", "none");
+	});
+	$("#undisplay-phone-operating-sys-filter-btn").click( function() {
+		$(this).css("display", "none");
+		$("#display-phone-operating-sys-filter-btn").css("display", "block");
+		$("#phone-operating-sys-filter").css("display", "none");
+	});
+
+
+	$("#display-acc-type-filter-btn").click( function() {
+		$(this).css("display", "none");
+		$("#undisplay-acc-type-filter-btn").css("display", "block");
+		$("#acc-type-filter").css("display", "block");
+	});
+	$("#display-acc-price-filter-btn").click( function() {
+		$(this).css("display", "none");
+		$("#undisplay-acc-price-filter-btn").css("display", "block");
+		$("#acc-price-filter").css("display", "block");
+	});
+
+	$("#undisplay-acc-type-filter-btn").click( function() {
+		$(this).css("display", "none");
+		$("#display-acc-type-filter-btn").css("display", "block");
+		$("#acc-type-filter").css("display", "none");
+	});
+	$("#undisplay-acc-price-filter-btn").click( function() {
+		$(this).css("display", "none");
+		$("#display-acc-price-filter-btn").css("display", "block");
+		$("#acc-price-filter").css("display", "none");
+	});
+
+	/*Xử lý hiển thị khi click vào nút phụ kiện, điện thoại trong khung bộ lọc*/
+	$("#phone-filter-btn").click( function() {
+		$(this).css("background-color", "white");
+		$(this).css("color", "#05a90c");
+		$(this).css("font-weight", "bold");
+		$("#accessories-filter-btn").css("background-color", "white");
+		$("#accessories-filter-btn").css("color", "black");
+		$("#accessories-filter-btn").css("font-weight", "normal");
+		$("#phone-filter").css("display", "block");
+		$("#accessories-filter").css("display", "none");
+	});
+	$("#accessories-filter-btn").click( function() {
+		$(this).css("background-color", "white");
+		$(this).css("color", "#05a90c");
+		$(this).css("font-weight", "bold");
+		$("#phone-filter-btn").css("background-color", "white");
+		$("#phone-filter-btn").css("color", "black");
+		$("#phone-filter-btn").css("font-weight", "normal");
+		$("#phone-filter").css("display", "none");
+		$("#accessories-filter").css("display", "block");
+	});
+
+	/*Xử lý khung đăng nhập, đăng ký*/
+	$("#logIn-btn-option").click( function() {
+		$("#logIn-form").css("display", "block");
+		$("#signIn-form").css("display", "none");
+	});
+	$("#signIn-btn-option").click( function() {
+		$("#logIn-form").css("display", "none");
+		$("#signIn-form").css("display", "block");
+	});
+
+
+	/*Hiển thị nhập bình luận khi click vào nút trả lời*/
+	$(".reply-comment-btn").click( function() {
+
+		$(this).closest(".comment").children(".comment-input-area").css("display", "block");
+
+		console.log($(this).closest(".comment").children(".comment-input-area"));
+
+	});
+	
 });
